049f3db78ee897345527ce6e07e897e7c7f77ff9
John Vilk <jvilk@cs.umass.edu>
2015-11-09 16:08:49 -0500

    Actually export all of the properties on the process object.
    Previously, we were ignoring a TON of properties, e.g. EventEmitter functions.

--- ts/index.ts
+++ ts/index.ts
@@ -1,19 +1,27 @@
 import Process = require('./process');
 import TTD = require('./tty');
-var process = new Process();
 
-// Manually export the individual public functions of Process.
-// Required because some code will invoke functions off of the module.
-// e.g. the `async` library sets `async.nextTick = process.nextTick`,
-// so the 'this' object is set to `async` when it is invoked.
-export var chdir = (dir: string) => process.chdir(dir);
-export var cwd = () => process.cwd();
-export var platform = process.platform;
-export var uptime = () => process.uptime();
-export var argv = process.argv;
-export var stdout = process.stdout;
-export var stderr = process.stderr;
-export var stdin = process.stdin;
-export var nextTick = function() {
-  return process.nextTick.apply(process, arguments);
-};
+var process = new Process(),
+  processProxy: Process = <any> {};
+
+function defineKey(key: string) {
+  if ((<any> processProxy)[key]) {
+    // Probably a builtin Object property we don't care about.
+    return;
+  }
+  if (typeof (<any> process)[key] === 'Function') {
+    (<any> processProxy)[key] = function() {
+      return (<Function> (<any> process)[key]).apply(process, arguments);
+    };
+  } else {
+    (<any> processProxy)[key] = (<any> process)[key];
+  }
+}
+
+for (var key in process) {
+  // Don't check if process.hasOwnProperty; we want to also expose objects
+  // up the prototype hierarchy.
+  defineKey(key);
+}
+
+export = processProxy;
